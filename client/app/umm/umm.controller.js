8df8d134019ea561a0955ad86a42413df6f1f23c
Brian Mitchell <bman4789@gmail.com>
2014-09-20 16:12:12 -0500

    The list of movies is now updated when a new movie is added or deleted

--- client/app/umm/umm.controller.js
+++ client/app/umm/umm.controller.js
@@ -4,20 +4,29 @@ angular.module('yeoMeanApp')
   .controller('UmmCtrl', function ($scope, $http) {
     $scope.movieList = [];
 
+    //Update movieList to have the same data that's in the database on the sever
     $http.get('/api/movies').success(function(movieList) {
         $scope.movieList = movieList;
     });
 
     $scope.addMovie = function() {
-        if($scope.newMovie === '') {
+        if($scope.newMovie === ''  || $scope.newRating === '') {
             return;
         }
         $http.post('/api/movies', { name: $scope.newMovie, rating: $scope.newRating });
         $scope.newMovie = '';
         $scope.newRating = '';
+        //Update movieList to have the same data that's in the database on the sever
+        $http.get('/api/movies').success(function(movieList) {
+            $scope.movieList = movieList;
+        });
     };
 
     $scope.deleteMovie = function(movie) {
         $http.delete('/api/movies/' + movie._id);
+        //Update movieList to have the same data that's in the database on the sever
+        $http.get('/api/movies').success(function(movieList) {
+            $scope.movieList = movieList;
+        });
     };
   });
