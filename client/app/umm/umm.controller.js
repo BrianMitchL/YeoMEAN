446417ed080b8761a16aeefc0e15ce4def0cc991
Brian Mitchell <bman4789@gmail.com>
2014-09-25 12:42:02 -0500

    Got editing to work!

--- client/app/umm/umm.controller.js
+++ client/app/umm/umm.controller.js
@@ -32,17 +32,13 @@ angular.module('yeoMeanApp')
         });
     };
 
-    $scope.editMovie = function() {
-        if($scope.newMovie === ''  || $scope.newRating === '') {
-            return;
-        }
-        $http.post('/api/movies', { name: $scope.newMovie, rating: $scope.newRating }).success(function(){
+    $scope.updateMovie = function(movie, $parent) {
+        $parent.isEditing = false;
+        $http.put('/api/movies/' + movie._id, movie).success(function(){
             //Update movieList to have the same data that's in the database on the sever
             $http.get('/api/movies').success(function(movieList) {
                 $scope.movieList = movieList;
             });
-            $scope.newMovie = '';
-            $scope.newRating = '';
         });
     };
   });
