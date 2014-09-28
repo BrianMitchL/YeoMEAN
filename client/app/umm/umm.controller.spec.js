0d5d7a64435ba8cecfb1f320acd7f055a7749516
Brian Mitchell <bman4789@gmail.com>
2014-09-28 15:03:33 -0500

    All string use single quotes and unused function inputs were removed to comply with grunt default tasks.

--- client/app/umm/umm.controller.spec.js
+++ client/app/umm/umm.controller.spec.js
@@ -16,14 +16,14 @@ describe('Controller: UmmCtrl', function () {
     });
     movieList = [];
       //The following httpBackend things will determine what will happen when an http request is called during our tests
-    httpBackend.when('POST', '/api/movies').respond(function(method, url, data, headers) {
+    httpBackend.when('POST', '/api/movies').respond(function(method, url, data) {
         movieList.push(JSON.parse(data));
         return [200, {}, {}];
     });
     httpBackend.when('GET', '/api/movies').respond(movieList);
       //The 1 is for identifying the _id on the movie in the "database"
     httpBackend.when('DELETE', '/api/movies/1').respond(function() {
-        movieList.splice(movieList.indexOf({name:"Batman", rating:9}),1);
+        movieList.splice(movieList.indexOf({name:'Batman', rating:9}),1);
         return [200, {}, {}];
     });
   }));
@@ -34,20 +34,20 @@ describe('Controller: UmmCtrl', function () {
 
   describe('testing add and delete from the database', function() {
       it('should add a new movie "Batman" with a rating of 9 and return it', function() {
-          scope.newMovie = "Batman";
+          scope.newMovie = 'Batman';
           scope.newRating = 9;
           scope.addMovie();
           //all of the http requests are added into a queue and are all run when the flush() method is called
           httpBackend.flush();
-          expect(movieList[0]).toEqual({name:"Batman", rating:9});
+          expect(movieList[0]).toEqual({name:'Batman', rating:9});
       });
 
       it('should return empty object', function() {
-          scope.newMovie = "Batman";
+          scope.newMovie = 'Batman';
           scope.newRating = 9;
           scope.addMovie();
           //The _id is used so the correct movie is deleted from the "database"
-          scope.deleteMovie({name:"Batman", rating:9, _id:1});
+          scope.deleteMovie({name:'Batman', rating:9, _id:1});
           httpBackend.flush();
           expect(movieList.length).toEqual(0);
           expect(movieList[0]).toEqual(undefined);
