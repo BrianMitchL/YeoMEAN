d539ea95b3b973c94c829f3dc1a75f2993975294
Brian Mitchell <bman4789@gmail.com>
2014-11-22 01:54:02 -0600

    Minor styling tweaks

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('yeoMeanApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Angular Home',
       'link': '/'
     },
     {'title': 'Movies',
