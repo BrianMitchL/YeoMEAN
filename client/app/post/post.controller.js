0e0e2599e5124cfe79f0ddd4eab681fa0399f020
Brian Mitchell <bman4789@gmail.com>
2014-12-02 10:56:07 -0600

    Kinda hacky 404 if a post url is loaded when it doesn't exist in the posts.json file

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -4,6 +4,10 @@ angular.module('yeoMeanApp')
   .controller('PostCtrl', function ($rootScope, $scope, $routeParams, $filter, Posts) {
     Posts.getPosts(function(data){
       $scope.post = $filter('filter')(data, {slug: $routeParams.id})[0];
+        if(($filter('filter')(data, {slug: $routeParams.id})[0]) == undefined) {
+            $scope.post = {"title":"404", "date":"This page does not exist, please check your URL and try again"};
+            $scope.error = true;
+        }
       $rootScope.header = "Brian Mitchell | " + $scope.post.title;
     });
   });
