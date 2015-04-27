01a02f83111ba707f250675aad068f9fb85ec583
Brian Mitchell <bman4789@gmail.com>
2015-04-27 00:13:17 -0500

    Use ngTable for the movies page

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('yeoMeanApp', [
   'ngRoute',
   'ui.bootstrap',
   'ngAnimate',
-  'mvd.comments'
+  'mvd.comments',
+  'ngTable'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
--- client/app/movies/movies.controller.js
+++ client/app/movies/movies.controller.js
@@ -1,71 +1,72 @@
 'use strict';
 
 angular.module('yeoMeanApp')
-  .controller('MoviesCtrl', function ($rootScope, $scope, $http, $log) {
+  .controller('MoviesCtrl', function ($rootScope, $scope, $http, $filter, ngTableParams) {
     $rootScope.metaTitle = "Movies | Brian Mitchell";
     $rootScope.metaDescription = "A movie ratings page";
     $rootScope.metaType = "website";
     $rootScope.metaImage = "/assets/images/BM-Logo-Large.png";
-    $scope.movieList = [];
-    $scope.displayList = [];
-    $scope.totalItems = 0;
-    $scope.currentPage = 1;
-    $scope.itemsPerPage = 10;
+    var data = [];
 
-    getMovies();
+    $http.get('/api/movies').success(function(movieList) {
+      data = movieList;
+      $scope.movieParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        sorting: {
+          name: 'asc'     // initial sorting (ascending)
+        }
+      }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+          // use build-in angular filter
+          var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data; //ordered array of objects based on filter
 
-    $scope.pageChanged = function() {
-      var index = ($scope.currentPage - 1) * $scope.itemsPerPage;
-      var numOnPage = $scope.itemsPerPage;
-      if ($scope.totalItems - index < $scope.itemsPerPage) {
-          numOnPage = $scope.totalItems - index;
-      }
-      $scope.displayList = [];
-      for (var i = 0; i < numOnPage; i++) {
-          $scope.displayList[i] = $scope.movieList[index + i];
-      }
-      //Show a full table on the last page if it's not full
-      //if (numOnPage != $scope.itemsPerPage) {
-      //    for (var i = numOnPage; i < $scope.itemsPerPage; i++) {
-      //      $scope.displayList[i] = {};
-      //    }
-      //}
-      //$log.log('Page changed to: ' + $scope.currentPage);
-    };
+          params.total(orderedData.length); // set total for recalc pagination
+          $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+      });
+    }).error(function(data, status) {
+      console.log(status + ': ' + data);
+    });
 
     $scope.addMovie = function() {
-        if($scope.newMovie === ''  || $scope.newRating === '') {
-            return;
-        }
-        $http.post('/api/movies', { name: $scope.newMovie, rating: $scope.newRating }).success(function(){
-            getMovies();
-            $scope.newMovie = '';
-            $scope.newRating = '';
-        });
+      if($scope.newMovie === ''  || $scope.newRating === '') {
+        return;
+      }
+      $http.post('/api/movies', { name: $scope.newMovie, rating: $scope.newRating }).success(function(){
+        getMovies();
+        $scope.newMovie = '';
+        $scope.newRating = '';
+      });
     };
 
     $scope.deleteMovie = function(movie) {
-        $http.delete('/api/movies/' + movie._id).success(function(){
-            getMovies();
-        });
+      $http.delete('/api/movies/' + movie._id).success(function(){
+        getMovies();
+      }).error(function(data, status) {
+        console.log(status + ': ' + data);
+      });
     };
 
     $scope.updateMovie = function(movie) {
-        $scope.isEditing = false;
-        $http.put('/api/movies/' + movie._id, movie).success(function(){
-            getMovies();
-        });
-    };
-
-    $scope.editMovie = function(movie) {
-        movie.isEditing = true;
+      $http.put('/api/movies/' + movie._id, movie).success(function(){
+        getMovies();
+      }).error(function(data, status) {
+        console.log(status + ': ' + data);
+      });
     };
 
     function getMovies() {
       $http.get('/api/movies').success(function(movieList) {
-        $scope.movieList = movieList;
-        $scope.totalItems = $scope.movieList.length;
-        $scope.pageChanged();
+        data = movieList;
+        $scope.movieParams.reload();
+      }).error(function(data, status) {
+        console.log(status + ': ' + data);
       });
+    }
+
+    $scope.reloadMovies = function() {
+      getMovies();
     }
   });
